29d7bbcaa6022363310e6f55aa5e79bdaa15969c
Wonjin Cho <cwonjin1@gmail.com>
2021-12-21 10:49:28 -0500

    ReadMe update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # freecodecamp-my-solution
+* This is to expand my knowledge of Javascript DS&A problem solving skills.
+
+# Currently added solution
+* Sum all numbers in a range
+
+
--- Sum-All-Numbers-in-a-Range/solution.js
+++ Sum-All-Numbers-in-a-Range/solution.js
@@ -1,8 +1,27 @@
 const sumAll = (arr) =>  {
   
-    let min = 0
-    console.log(min)
+    // create varibles for max and min 
+    let min, max
+    
+    // define min and max
+    if (arr[0] > arr[1]){
+        min = arr[1]
+        max = arr[0]
+    } else {
+        min = arr[0]
+        max = arr[1]
+    }
+
+    // sum all elements in an array
+
+    //create a variable sum equals to 0 to initialize
+    let sum = 0
+
+    //looping through
+    for(let i=min; i<=max; i++){
+       sum += i
+    }
+    return sum
 
 }
-
-sumAll([1, 4]);
+console.log(sumAll([1, 10]))
