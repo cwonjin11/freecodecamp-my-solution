836a89421f03eaf55b999ae93489c6fc871ee58e
Wonjin Cho <cwonjin1@gmail.com>
2021-12-29 12:04:39 -0500

    Using 1.for loop, 2. object, 3. ternary operator

---
+++ 11.Convert-HTML-Entities/refactor.js
@@ -0,0 +1,29 @@
+const htmlDict = {
+    '&':"&amp;",
+    '<':"&lt;",
+    '>':"&gt;",
+    '"':"&quot;",
+    "'":"&apos;"
+}
+
+
+const convertHTML = (str) => {
+
+    let output = ''
+
+    for(const char of str){
+        //using ternary operator
+        // if char in htmlDict is true, then append htmldict add key char, if not append char itself
+        output += char in htmlDict ? htmlDict[char] : char
+
+
+        // if(char in htmlDict){
+        //     output += htmlDict[char]
+        // }else {
+        //     output += char
+        // }
+    }
+    return output
+}
+  
+  console.log(convertHTML("Dolce & Gabbana"));
---
+++ 11.Convert-HTML-Entities/solution.js
@@ -0,0 +1,25 @@
+const convertHTML = (str) => {
+
+    //create a varialbe output as empty string
+    let output = ''
+
+    // for loop 
+    for(const char of str){
+        if(char === '&'){
+          output += "&amp;"
+        }else if (char === '<'){
+            output += "&lt;"
+        }else if (char === '>'){
+            output += "&gt;"
+        }else if (char === '"'){
+            output += "&quot;"
+        }else if (char === "'"){
+            output += "&apos;"
+        }else {
+            output += char
+        }
+    }
+    return output;
+}
+  
+  console.log(convertHTML("Dolce & Gabbana"));
