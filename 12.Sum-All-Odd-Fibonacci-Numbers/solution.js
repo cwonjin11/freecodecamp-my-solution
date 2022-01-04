df7a3ee439a0c461147ed0c3f4aedf362e42888f
Wonjin Cho <cwonjin1@gmail.com>
2022-01-04 14:25:04 -0500

    Added: solution using while loop and temp variable

--- 12.Sum-All-Odd-Fibonacci-Numbers/solution.js
+++ 12.Sum-All-Odd-Fibonacci-Numbers/solution.js
@@ -1,11 +1,38 @@
 const sumFibs = (num) => {
   
-    let sum = 0
-    
-  
+    let a = 1
+    let b = 1
+    let sum = a
+
+    while(b<=num){
+        if(b%2 !== 0){
+             sum += b
+        }
+        
+        let temp = b
+        b = a + b
+        a = temp  // not a = b because I want the old value of b    
+        //or [a,b] = [b, a+b]
+    }
     return sum
+}
   
-  }
-  
-  sumFibs(4);
-  //1,1,2,3 => 1,1,3 => 5
+
+    //1,1,2,3 => 1,1,3 => 5
+
+
+  console.log(sumFibs(4));          
+  console.log(sumFibs(1000));
+  console.log(sumFibs(4000000));    
+  console.log(sumFibs(75024));
+  console.log(sumFibs(75025));
+
+
+  /*
+  5
+1785
+4613732
+60696
+135721
+   */
+
