70ea7f04ccc27618cf4b40f0670929579789fbff
Wonjin Cho <cwonjin1@gmail.com>
2022-01-04 15:01:48 -0500

    Added: two solutions added.

--- 13.Sum-All-Primes/solution2.js
+++ 13.Sum-All-Primes/solution2.js
@@ -1,9 +1,9 @@
 const sumPrimes = (num) => {
 
     //edge case
-    if(num < 2){
-        return 0
-    }
+    // if(num < 2){
+    //     return 0
+    // }
 
     const primeNumbers = [];
     for(let i=2; i <= num; i++){
@@ -14,19 +14,21 @@ const sumPrimes = (num) => {
                 break
             }
         }
-
         if (!isComposite) {
             primeNumbers.push(i)
         }
     }
-
-    // console.log(primeNumbers)
-
-    let sum = 0
-    for (const p of primeNumbers){
-        sum += p
-    }
-
+    //refactor using reduce
+    //accumulator as a function
+    //p is what we are iterating over
+    //0 is a default value
+    //acc + p is a result 
+    let sum = primeNumbers.reduce((acc, p) => acc + p, 0)
+    // [2,3,5,7]
+    // 0 + 2 = 2
+    // 2 + 3 = 5
+    // 5 + 5 = 10
+    // 10 + 7 = 17
     return sum;
   }
   
