bdd7c1538e257604f3643c9c1e6090f97bdd4891
Wonjin Cho <cwonjin1@gmail.com>
2022-02-09 15:56:52 -0500

    Added: using hashmap and stack to check true or false

---
+++ 0.practice_algo_javascript/10.valid_parenthesis.js
@@ -0,0 +1,23 @@
+function validParentheses(s) {
+
+    const hashMap = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+
+    const stack = []
+    for (let char of s){
+        if(hashMap[char]){
+            console.log(hashMap[char])
+            stack.push(hashMap[char])
+        } else if(stack.length > 0 && stack[stack.length-1] === char) {
+            stack.pop()
+        } else {
+            return false
+        }
+    }
+    return stack.length === 0
+}
+
+console.log(validParentheses("()[]{}"))
