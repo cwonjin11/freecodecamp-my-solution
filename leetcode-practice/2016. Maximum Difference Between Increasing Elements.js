443d1b9a73e7a82e314a43cb5a08be9293284a86
Wonjin Cho <cwonjin1@gmail.com>
2022-02-11 16:45:17 -0500

    exercise solved

---
+++ leetcode-practice/2016. Maximum Difference Between Increasing Elements.js
@@ -0,0 +1,16 @@
+var maximumDifference = function(nums) {
+    
+    let result = -1
+    
+    for(let i=0; i < nums.length-1; i++){
+        for(let j=i+1; j < nums.length; j++){
+             let differ = nums[j] - nums[i]
+             if(i<j && nums[i] < nums[j] && differ > result){
+                result = differ
+            }
+        }
+    }
+    return result
+};
+
+
