0b8c29ba9f3fb1f9654cf39748f8bb1d590831c8
Wonjin Cho <cwonjin1@gmail.com>
2022-02-17 13:02:03 -0500

    Exercise completed

---
+++ leetcode-practice/345. Reverse Vowels of a String.js
@@ -0,0 +1,27 @@
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+
+ 
+
+// Example 1:
+
+// Input: s = "hello"
+// Output: "holle"
+// Example 2:
+
+// Input: s = "leetcode"
+// Output: "leotcede"
+ 
+
+var reverseVowels = function(s) {
+  
+    // let result = []
+    // let vowels = ["a", "e", "i", "o", "u"]
+
+    // for (let i = 0; i < s.length; i++){
+    //     if()
+    // }
+};
+
+console.log(reverseVowels("hello")) // "holle"
