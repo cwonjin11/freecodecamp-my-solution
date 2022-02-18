d141bbccf09079e3110e742fa61c16d6cfadf2c9
Wonjin Cho <cwonjin1@gmail.com>
2022-02-18 14:27:53 -0500

    solved

---
+++ leetcode-practice/1133. Largest Unique Number.js
@@ -0,0 +1,51 @@
+// Given an integer array nums, return the largest integer that only occurs once. If no integer occurs once, return -1.
+
+// Example 1:
+
+// Input: nums = [5,7,3,9,4,9,8,3,1]
+// Output: 8
+// Explanation: The maximum integer in the array is 9 but it is repeated. The number 8 occurs only once, so it is the answer.
+// Example 2:
+
+// Input: nums = [9,9,8,8]
+// Output: -1
+// Explanation: There is no number that occurs only once.
+
+
+// var largestUniqueNumber = function(nums) {  
+//     let map = {}
+//     let modifiedArray = [-1]
+
+//     for(const num of nums){
+//        !map[num] ? map[num] = 1 : map[num]++
+//     }
+//     console.log(map)
+
+//     for(const num in map){
+//         if(map[num] === 1){
+//             modifiedArray.push(num)
+//         }
+//     }
+//     return Math.max(...modifiedArray) 
+// };
+
+var largestUniqueNumber = function(nums) {
+let map = {}
+let maxNum = -1
+
+for(const num of nums){
+    !map[num] ? map[num] = 1 : map[num] ++
+}
+console.log(map)
+
+for(const num in map){
+    if(map[num] === 1){
+        maxNum = Math.max(maxNum, num)
+    }
+}
+return maxNum 
+}
+
+
+console.log(largestUniqueNumber([5,7,3,9,4,9,8,3,1]))
+console.log(largestUniqueNumber([9,9,8,8]))
---
+++ leetcode-practice/1748. Sum of Unique Elements.js
@@ -0,0 +1,46 @@
+// You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+// Return the sum of all the unique elements of nums.
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,2]
+// Output: 4
+// Explanation: The unique elements are [1,3], and the sum is 4.
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: 0
+// Explanation: There are no unique elements, and the sum is 0.
+// Example 3:
+
+// Input: nums = [1,2,3,4,5]
+// Output: 15
+// Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+
+var sumOfUnique = function(nums) {
+    
+    let map = {}
+    let sum = 0
+    
+    for(const num of nums){
+        // if(!map[num]){
+        //     map[num] = 1
+        // } else {
+        //     map[num]++
+        // }
+        !map[num] ? map[num] = 1 : map[num]++
+    }
+    
+    for(const key in map){
+        if(map[key] === 1) {
+            // sum += parseInt(key)
+            sum += +key
+        }
+    }
+    return sum
+    
+};
+
+console.log(sumOfUnique([1,2,3,2]))
