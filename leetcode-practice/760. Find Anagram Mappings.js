f741fb048e9da659b28f6d71f9e38b27360e2b19
Wonjin Cho <cwonjin1@gmail.com>
2022-02-19 12:42:13 -0500

    solved

---
+++ leetcode-practice/760. Find Anagram Mappings.js
@@ -0,0 +1,12 @@
+var anagramMappings = function(nums1, nums2) {
+    
+    // create variable output = []
+// create hash key=>nums2[i], value =>i
+    // loop nums1[i] from hash that matched to key
+    // push output array a hash[nums1[i]]
+    
+    
+    
+
+
+};
