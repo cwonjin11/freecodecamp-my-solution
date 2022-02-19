a8b6401ffb041bc4395a8aae806b2067093a7b29
Wonjin Cho <cwonjin1@gmail.com>
2022-02-19 12:48:02 -0500

    solved

--- leetcode-practice/760. Find Anagram Mappings.js
+++ leetcode-practice/760. Find Anagram Mappings.js
@@ -6,7 +6,20 @@ var anagramMappings = function(nums1, nums2) {
     // push output array a hash[nums1[i]]
     
     
-    
+    let map = {}
+    let output = []
+    for(let i = 0 ; i < nums2.length; i++){
+        map[nums2[i]] = i
+    }
+    console.log(map)
 
+    for(let i = 0; i < nums1.length; i++){
+        // console.log(map[nums1[i]])
+        output.push(map[nums1[i]])
+    }
+    return output
 
 };
+
+// console.log(anagramMappings([12,28,46,32,50], [50,12,32,46,28])) //=> [1,4,3,2,0]
+console.log(anagramMappings([40, 40], [40, 40])) //=> [1,4,3,2,0]
