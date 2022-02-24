f0e71a8c753c5fe26f0edf791ead5a4454f9f2de
Wonjin Cho <cwonjin1@gmail.com>
2022-02-24 11:37:24 -0500

    solved

---
+++ leetcode-practice/easy level/977. Squares of a Sorted Array.js
@@ -0,0 +1,51 @@
+// Given an integer array nums sorted in non-decreasing order, 
+// return an array of the squares of each number sorted in non-decreasing order.
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+
+var sortedSquares = function(nums) {
+    
+  let left = 0, right = nums.length -1
+  let result = []
+
+  while (left <= right){
+      let leftSquares = nums[left] * nums[left]
+      let rightSquares = nums[right] * nums[right]
+
+      if(leftSquares > rightSquares){
+          result.push(leftSquares)
+          left++
+      } else {
+        result.push(rightSquares)
+        right--
+      }
+  }
+  return result.sort((a,b) => a-b)
+
+};
+
+console.log(sortedSquares([-4,-1,0,3,10])) // [0,1,9,16,100]
+console.log(sortedSquares([-7,-3,2,3,11])) // [4,9,9,49,121]
+
+// var sortedSquares = function(nums) {
+    
+//     let output = []
+
+//     for(let i = 0; i < nums.length; i++){
+//         let squareNum = nums[i] * nums[i]
+//         output.push(squareNum)
+//     }
+//     return output.sort((a,b) => a - b)
+// };
+
+// console.log(sortedSquares([-4,-1,0,3,10])) // [0,1,9,16,100]
+// console.log(sortedSquares([-7,-3,2,3,11])) // [4,9,9,49,121]
