2881028518bb24d26ce53f552c8a0271accd7301
Wonjin Cho <cwonjin1@gmail.com>
2022-03-04 15:57:16 -0500

    solved

--- leetcode-practice/easy level/1365. How Many Numbers Are Smaller Than the Current Number.js
+++ leetcode-practice/easy level/1365. How Many Numbers Are Smaller Than the Current Number.js
@@ -1,17 +1,31 @@
+//brute force way but much readable
 var smallerNumbersThanCurrent = function(nums) {
     
     let res = []
-     
- for(let i = 0; i < nums.length; i++){
-     let count = 0 
-     for(let j = 0 ; j < nums.length; j++){
-         if(nums[i] > nums[j]){
-             count++
-         }
-     }
-     res.push(count)
- }
+    for(let i = 0; i < nums.length; i++){
+        let count = 0 
+        for(let j = 0 ; j < nums.length; j++){
+            if(nums[i] > nums[j]){
+                count++
+            }
+        }
+        res.push(count)
+    }
     return res
+};
+
+
+//more efficient
+var smallerNumbersThanCurrent = function(nums) {
+    
+    let result = []
+    let arr = [...nums]
+    nums.sort((a,b) => a-b)
+    
+    for(let i = 0; i < arr.length; i++){
+        result.push(nums.indexOf(arr[i]))
+    }
+    return result
 };
 
 
