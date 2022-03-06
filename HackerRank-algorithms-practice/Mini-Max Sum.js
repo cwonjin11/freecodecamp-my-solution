e11dbc813a3bd61daa1bedc35bd439416758e3ef
Wonjin Cho <cwonjin1@gmail.com>
2022-03-05 20:55:39 -0500

    solved

--- HackerRank-algorithms-practice/Mini-Max Sum.js
+++ HackerRank-algorithms-practice/Mini-Max Sum.js
@@ -1,4 +1,6 @@
 function miniMaxSum(arr) {
+
+
     // Write your code here
     
 //     let minArray = []
@@ -32,3 +34,18 @@ function miniMaxSum(arr) {
 
 
 console.log(miniMaxSum([1,3,5,7,9]))
+
+// since arr len is fixed
+function miniMaxSum(arr) {
+    // Write your code here
+    arr = arr.sort((a,b) => a - b)
+    let minSum = 0
+    let maxSum = 0
+    
+    for(let i = 0; i < arr.length; i++){
+        minSum = arr[0] + arr[1] + arr[2] + arr[3]
+        maxSum = arr[1] + arr[2] + arr[3] + arr[4]
+    }
+    
+    console.log(minSum + " " + maxSum)
+}
