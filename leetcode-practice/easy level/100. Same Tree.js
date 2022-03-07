7204661c97c5d4b3c08b0f8fbfe9d28ec1f43545
Wonjin Cho <cwonjin1@gmail.com>
2022-03-07 14:52:10 -0500

    solved

--- leetcode-practice/easy level/100. Same Tree.js
+++ leetcode-practice/easy level/100. Same Tree.js
@@ -21,16 +21,6 @@
 
 
 
-//   function TreeNode(val, left, right) {
-//       this.val = (val===undefined ? 0 : val)
-//       this.left = (left===undefined ? null : left)
-//       this.right = (right===undefined ? null : right)
-//  }
-
-//  @param {TreeNode} p
-//  @param {TreeNode} q
-//  @return {boolean}
-
 
 var isSameTree = function(p, q) {
     
