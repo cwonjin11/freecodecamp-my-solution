2e54a106c681143d6a005c0b810632903977f83a
Wonjin Cho <cwonjin1@gmail.com>
2022-03-16 11:04:00 -0400

    added

---
+++ leetcode-practice/easy level/00.practice.js
@@ -0,0 +1,29 @@
+
+/**
+ * arr1.reverse() mutates original array
+ */
+let arr1 = [1,2,3,4,5]
+let reversed1 = arr1.reverse()
+
+console.log(arr1) // [ 5, 4, 3, 2, 1 ]
+console.log(reversed1) // [ 5, 4, 3, 2, 1 ]
+/**
+ * To keep the original array as it is, use spread operator.
+ */
+let arr2 = [1,2,3,4,5]
+let reversed2 = [...arr2].reverse()
+
+console.log(arr2) // [ 1, 2, 3, 4, 5 ]
+console.log(reversed2) // [ 5, 4, 3, 2, 1 ]
+
+/**
+ * Check to see if certain element is in the array. 
+ * 1. use if else conditinoal state with "||" . Not best practice 
+ * 2. includes method // good!
+ * 3. use Hash set and use has method // awesome!
+ */
+
+let set = new Set(arr2)
+    if(set.has(6)){
+       console.log(true)
+    } else {console.log(false)}
