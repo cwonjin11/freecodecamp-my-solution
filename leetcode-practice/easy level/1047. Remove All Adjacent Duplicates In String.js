d292bb55127f4be798fcb1232277dcb41abad66a
Wonjin Cho <cwonjin1@gmail.com>
2022-03-21 10:57:00 -0400

    solved

---
+++ leetcode-practice/easy level/1047. Remove All Adjacent Duplicates In String.js
@@ -0,0 +1,17 @@
+var removeDuplicates = function(s) {
+
+   let stack = []
+
+   for(const char of s){
+       if(char == stack[stack.length - 1]){
+           stack.pop(char)
+       } else {
+           stack.push(char)
+       }
+   }
+   console.log(stack)
+   return stack.join("")
+
+}
+
+console.log(removeDuplicates("abbaca")) // ca
