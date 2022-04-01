c1e123a6c6d9df3302c6ad8a650e23eb30d4df99
Wonjin Cho <cwonjin1@gmail.com>
2022-04-01 15:10:52 -0400

    solved

---
+++ self_practice.js/trim().js
@@ -0,0 +1,7 @@
+
+//trim() is a string method to remove whitespace from both ends of a string and returns a new string
+const greeting = "       Hello    Word!!        "
+console.log(greeting, "original")
+const trimmed = greeting.trim()
+console.log(trimmed)
+console.log(greeting, "without modifying original")
