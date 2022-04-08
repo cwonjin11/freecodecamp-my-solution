ad36e081c8e000a9b5d2909adce1c2f5d2ece9cc
Wonjin Cho <cwonjin1@gmail.com>
2022-04-08 16:26:09 -0400

    added

--- free-code-camp/Repeat a String Repeat a String.js
+++ free-code-camp/Repeat a String Repeat a String.js
@@ -12,3 +12,18 @@ function repeatStringNumTimes(str, num) {
   }
   
   repeatStringNumTimes("abc", 3);
+
+  
+  //more concise
+  function repeatStringNumTimes(str, num) {
+
+    let output = ""
+  
+      for(let i = 0; i < num; i++){
+        output += str
+       }
+  
+    return output;
+  }
+  
+  repeatStringNumTimes("abc", 3);
