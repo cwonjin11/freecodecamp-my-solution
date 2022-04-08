de75a2161a258547335d2bfa21c7e99c35d991fe
Wonjin Cho <cwonjin1@gmail.com>
2022-04-08 16:36:09 -0400

    added

--- free-code-camp/Finders Keepers.js
+++ free-code-camp/Finders Keepers.js
@@ -10,3 +10,15 @@ function findElement(arr, func) {
   
 findElement([1, 2, 3, 4], num => num % 2 === 0);
 
+
+// more readable (coherent)
+function findElement(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+      if (func(arr[i])) {
+        return arr[i];
+      }
+    }
+    return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
