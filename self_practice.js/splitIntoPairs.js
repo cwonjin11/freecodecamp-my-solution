d19a45dd9f6b08179a1dc2141656f2cf6f5bdb4d
Wonjin Cho <cwonjin1@gmail.com>
2022-05-15 19:36:05 -0400

    Optimized solution

--- self_practice.js/splitIntoPairs.js
+++ self_practice.js/splitIntoPairs.js
@@ -1,24 +1,43 @@
 
-const splitIntoPairs = (str) => {
+// const splitIntoPairs = (str) => {
 
-    let result = []
-    let arr = str.split("")
+//     let result = []
+//     let arr = str.split("")
 
    
-    if(arr.length == 1) {
-        arr.push("_")
-    }
+//     if(arr.length == 1) {
+//         arr.push("_")
+//     }
 
-    while(arr.length > 1){
-        let first2 = arr.splice(0,2)
-        result.push(first2.join(""))
-        if(arr.length == 1){
-            arr.push("_")
-        }
-   }
-   return result
+//     while(arr.length > 1){
+//         let first2 = arr.splice(0,2)
+//         result.push(first2.join(""))
+//         if(arr.length == 1){
+//             arr.push("_")
+//         }
+//    }
+//    return result
+// }
+
+const splitIntoPairs = (str) => {
+
+    let i = 0
+    let result = []
+
+    if(str.length % 2 !== 0){
+       str += "_"
+    }
+    // console.log(str)
+    
+    while(i < str.length){
+        result.push(str[i] + str[i+1])
+        i = i + 2
+    }
+    
+    return result
 }
 
+
 console.log(splitIntoPairs("abcde")) //=>["ab", "cd", "e_"]
-console.log(splitIntoPairs("abcdef")) //=>["ab", "cd", "e_"]
-console.log(splitIntoPairs("abc")) //=>["ab", "cd", "e_"]
+// console.log(splitIntoPairs("abcdef")) //=>
+// console.log(splitIntoPairs("abc")) //=>
