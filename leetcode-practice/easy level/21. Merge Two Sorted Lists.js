221fda68fea393423baf166204ea1a6bc005d285
Wonjin Cho <cwonjin1@gmail.com>
2022-05-23 13:32:31 -0400

    solved

--- leetcode-practice/easy level/21. Merge Two Sorted Lists.js
+++ leetcode-practice/easy level/21. Merge Two Sorted Lists.js
@@ -1,70 +1,36 @@
-// You are given the heads of two sorted linked lists list1 and list2.
-
-// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
-
-// Return the head of the merged linked list.
-
- 
-
-// Example 1:
-
-
-// Input: list1 = [1,2,4], list2 = [1,3,4]
-// Output: [1,1,2,3,4,4]
-// Example 2:
-
-// Input: list1 = [], list2 = []
-// Output: []
-// Example 3:
-
-// Input: list1 = [], list2 = [0]
-// Output: [0]
- 
-
-// Constraints:
-
-// The number of nodes in both lists is in the range [0, 50].
-// -100 <= Node.val <= 100
-// Both list1 and list2 are sorted in non-decreasing order.
-
-
-
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
 
-var mergeTwoLists = function(list1, list2) {
-
+var mergeTwoLists = function(l1, l2) {
     let dummy = new ListNode()
     let tail = dummy
-
-    while(list1 !== null && list2 !== null){
-        if(list1.val < list2.val){
-            tail.next = list1
-            list1 = list1.next
+ 
+    while( l1 !== null && l2 !== null){
+        //select the smallest value from either linked list, then increment that list forward
+        if(l1.val < l2.val){
+            tail.next = l1
+            l1 = l1.next
         } else {
-            tail.next = list2
-            list2 = list2.next
+             tail.next = l2
+            l2 = l2.next
         }
-        tail = tail.next
+        tail = tail.next // and then I need to update tail pointer regardless of which node we insert into our list
     }
-
-    if(list1 !== null){
-        tail.next = list1
+    
+    // what if the one list is empty but the other one isn't.
+    // we want to find non-empty list and insert it at the end of our result
+    if(l1 !== null){
+        tail.next = l1
     }
-    if(list2 !== null){
-        tail.next = list2
+    if(l2 !== null){
+        tail.next = l2
     }
-
-    return dummy.next
-
+    
+    console.log(dummy)
+    return dummy.next // because we dont want dummy node 0 for this case
+    
 };
+
