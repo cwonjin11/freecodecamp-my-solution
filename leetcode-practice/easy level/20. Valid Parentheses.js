adb8d754538db7af4d8c413fef1e760506ed1ee2
Wonjin Cho <cwonjin1@gmail.com>
2022-05-23 10:52:46 -0400

    solved

--- leetcode-practice/easy level/20. Valid Parentheses.js
+++ leetcode-practice/easy level/20. Valid Parentheses.js
@@ -1,22 +1,46 @@
-var isValid = function(s) {
+// var isValid = function(s) {
 
-    const map = { 
-        "(": ")",
-        "[": "]",
-        "{": "}"  
-    } 
+//     const map = { 
+//         "(": ")",
+//         "[": "]",
+//         "{": "}"  
+//     } 
     
-        let stack = [];
-        for(let char of s){ 
-            if(map[char]){ //if open parentheses present
-                stack.push(map[char])  //push value of char
-            } else { // if close parentheses present
-                if (stack.pop() !== char)
-                    return false
-            }   
+//         let stack = [];
+//         for(let char of s){ 
+//             if(map[char]){ //if open parentheses present
+//                 stack.push(map[char])  //push value of char
+//             } else { // if close parentheses present
+//                 if (stack.pop() !== char)
+//                     return false
+//             }   
+//         }
+//        return stack.length === 0
+// };
+
+
+var isValid = function(s) {
+    let hash = { 
+        "(": ")", 
+        "{": "}", 
+        "[": "]" }
+    
+    let stack = []
+    
+    for(const char of s){
+        if(hash[char]){
+            stack.push(hash[char])
+        } else if(stack[stack.length - 1] == char){
+                stack.pop()
+        } else {
+            return false
         }
-       return stack.length === 0
+    }
+    
+    return stack.length === 0
 };
+
+
 
 // console.log(isValid("()[]{}")) // true
 console.log(isValid("(]")) //false
--- leetcode-practice/easy level/206. Reverse Linked List.js
+++ leetcode-practice/easy level/206. Reverse Linked List.js
@@ -1,31 +1,67 @@
+ 
+//  function ListNode(val, next) {
+//      this.val = (val===undefined ? 0 : val)
+//      this.next = (next===undefined ? null : next)
+//  }
+
+
+
 var reverseList = function(head) {
     
-    let prev = null
-
-    while(head){ //head 1 2
-        const next = head.next  //next = 2 3
-        head.next = prev // 2 = prev  3 = prev
-        prev = head // 2 = head 3 head
-        head = next //head = 2 3
-    }
-    return prev
-    
-};
-
-//same solution. time complexity: O(n) (how many time do we iterate over out input)
-var reverseList = function(head) {
-
+    let prev = null // must store its previous element beforehand.
     let curr = head
-    let prev = null
-    let next = null
     
-    while( curr !== null){
-        next = curr.next
+    while(curr){ //(curr !== null)
+        let nextTemp = curr.next // need another pointer to store the next node before chaning the reference.
         curr.next = prev
         prev = curr
-        curr = next
+        curr = nextTemp
     }
     return prev
-
+    
+    // O(n), O(1);
 };
+
+
+// var reverseList = function(head) {
+    
+//     let prev = null
+
+//     while(head){ //head 1 2
+//         const next = head.next  //next = 2 3
+//         head.next = prev // 2 = prev  3 = prev
+//         prev = head // 2 = head 3 head
+//         head = next //head = 2 3
+//     }
+//     return prev
+    
+// }
+
+
+// const head = new ListNode(1);
+// const nodeB = head.next = new ListNode(2);
+// const nodeC = nodeB.next = new ListNode(3);
+// const nodeD = nodeC.next = new ListNode(4);
+// const nodeE = nodeD.next = new ListNode(5);
+// console.log(JSON.stringify(reverseList(head)));
+
+
+// console.log( ListNode([1,2,3,4,5]) )
+
+//same solution. time complexity: O(n) (how many time do we iterate over out input)
+// var reverseList = function(head) {
+
+//     let curr = head
+//     let prev = null
+//     let next = null
+    
+//     while( curr !== null){
+//         next = curr.next
+//         curr.next = prev
+//         prev = curr
+//         curr = next
+//     }
+//     return prev
+
+// };
 
