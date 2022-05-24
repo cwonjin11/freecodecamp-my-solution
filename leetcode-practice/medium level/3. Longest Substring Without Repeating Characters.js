7f95247a946fd4bc480da24383f33433e2267723
Wonjin Cho <cwonjin1@gmail.com>
2022-05-24 15:37:12 -0400

    solved

---
+++ leetcode-practice/medium level/3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function(s) {
+    
+    let set = new Set()
+    let longest = 0
+    let l = 0
+      
+    for (let r = 0; r < s.length; r++) {
+      while (set.has(s[r])) {
+        set.delete(s[l]);
+        l++;
+      }
+      set.add(s[r]);
+      longest = Math.max(longest, r - l + 1);
+    }
+    return longest;
+};
