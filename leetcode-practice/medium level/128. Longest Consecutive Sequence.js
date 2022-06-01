dd13b8a75a76af2aa24b4671873ba7300be5fc8d
Wonjin Cho <cwonjin1@gmail.com>
2022-06-01 13:12:17 -0400

    Longest consecutive sequence

---
+++ leetcode-practice/medium level/128. Longest Consecutive Sequence.js
@@ -0,0 +1,82 @@
+/*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+*/
+
+
+var longestConsecutive = function(nums) {
+    //edge case: no length or nums is null
+    if(nums.length === 0 || nums == null) return 0
+    
+    let set = new Set(nums)
+    console.log(set)
+    //main function
+   
+    let max = 0
+
+    for(const num of nums){
+        if(set.has(num - 1)){
+            continue
+        }
+        let currNum = num
+        let currMax = 1
+
+        while(set.has(currNum + 1)){
+            currNum++
+            currMax++
+        }
+        max = Math.max(max, currMax)
+    }
+    return max
+
+}
+
+// console.log(longestConsecutive([100,4,200,1,3,2]))
+
+
+var longestConsecutive = function(nums) {
+    
+    let set = new Set(nums)
+    let max = 0
+    
+    for( let num of nums){
+        if(!set.has(num-1)){ // set the starting point
+            let currMax = 0
+            while(set.has(num++)){
+                currMax++
+            }
+             max = Math.max(max, currMax)
+        }  
+       
+    }
+    return max
+    
+};
+
+
+var longestConsecutive = function(nums) {
+    
+    let set = new Set(nums)
+    let longest = 0
+    
+    for( let num of nums){
+        if(!set.has(num-1)){ // set the starting point
+            let length = 0
+            while(set.has(num + length)){
+                length++
+            }
+        longest = Math.max(longest, length)
+        }   
+    }
+    return longest
+};
+
