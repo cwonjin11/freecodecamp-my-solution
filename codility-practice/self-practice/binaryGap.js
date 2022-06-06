a669297f842736c9e9b86031667e2a2b9924e612
Wonjin Cho <cwonjin1@gmail.com>
2022-06-06 14:50:48 -0400

    binaryGap test case added

--- codility-practice/self-practice/binaryGap.js
+++ codility-practice/self-practice/binaryGap.js
@@ -2,6 +2,20 @@
 const binaryGap = (number) => {
 
     let biNum = number.toString(2)
-    console.log(biNum)
+  
+    let maxCount = 0
+    let currCount = 0
+
+    for(let num of biNum){
+        if(num == 0){
+            currCount++
+        } else {
+            maxCount = Math.max(currCount, maxCount)
+            currCount = 0
+        }
+    }
+    return maxCount
 }
 console.log(binaryGap(1041)) // 1000010001 => 5
+console.log(binaryGap(15)) // 1111 => 0
+console.log(binaryGap(32)) // 100000 => 0
