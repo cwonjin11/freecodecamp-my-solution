903a8ee564f5ad31ddc2be7ec242a8df5d9a3e6c
Wonjin Cho <cwonjin1@gmail.com>
2022-06-07 13:02:41 -0400

    added

--- 99.Binaray-Tree-Search practice/226. Invert Binary Tree.js
+++ 99.Binaray-Tree-Search practice/226. Invert Binary Tree.js
@@ -28,17 +28,50 @@ f.right = h
 
 
 
+// var invertTree = function(root) {
+//     if (root == null) return null
+
+//     //swap the children
+//      let right = invertTree(root.right)
+//      let left = invertTree(root.left)
+//      root.left = right
+//       root.right = left
+//    return root
+// };
+
+
+
+
 var invertTree = function(root) {
-    if (root == null) return null
+  
+   if(root === null) return []
 
-    //swap the children
-     let right = invertTree(root.right)
-     let left = invertTree(root.left)
-     root.left = right
-      root.right = left
-   return root
+   let result = []
+   const stack = [root]
+   while(stack.length > 0){
+      let current = stack.pop()
+      if(current){
+         [current.left, current.right] = [current.right, current.left]
+         result.push(current.left, current.right)
+      }
+   }
+   return result
 };
-
 
 console.log(invertTree(a))
   
+
+// // DFS
+// function invertTree(root) {
+//   const stack = [root];
+
+//   while (stack.length) {
+//     const n = stack.pop();
+//     if (n != null) {
+//       [n.left, n.right] = [n.right, n.left];
+//       stack.push(n.left, n.right);
+//     }
+//   }
+
+//   return root;
+// }
--- 99.Binaray-Tree-Search practice/binaryTree.js
+++ 99.Binaray-Tree-Search practice/binaryTree.js
@@ -11,6 +11,7 @@ const b = new Node("b")
 const c = new Node("c")
 const d = new Node("d")
 const e = new Node("e")
+const f = new Node("f")
 
 a.left = b
 a.right = c
