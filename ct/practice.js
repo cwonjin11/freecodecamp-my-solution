5cb6929bb94ff9709daf83847b3c1abb2629919b
Wonjin Cho <cwonjin1@gmail.com>
2022-06-17 09:55:20 -0400

    chnaged

--- ct/practice.js
+++ ct/practice.js
@@ -63,10 +63,45 @@ const burstBalloons = (points) => {
     return count
 }
 
-console.log(burstBalloons([[10,16], [2,4], [1,6], [7,12]]))
-console.log(burstBalloons([[10,16], [2,8], [1,9], [7,12]]))
-console.log(burstBalloons([[10,16]])) //1
-console.log(burstBalloons([[1,2], [3,4]])) // 2
-console.log(burstBalloons([[1,2], [2,4]])) //1
-console.log(burstBalloons([[]])) //0
+
+// best answer
+
+var findMinArrowShots = function(points) {
+   
+    points.sort((a,b) => a[1] - b[1])
+    //[ [2,4], [1,6], [7,12], [10,16] ]
+    let count = 1
+    let prevEnd = points[0][1] // 4 //6
+    
+     for(let i = 1; i < points.length; i++){
+         let start = points[i][0] //1 //7 //10
+         if( start <= prevEnd) continue // 1<4 t 7<=6? f 10<6 f
+         count++ // 2
+         prevEnd = points[i][1] //6
+     }
+     return count
+};
+
+//  intervals.sort((a,b) => a[1] - b[1])
+//  let count = 0
+//  let prev = intervals[0]  // set the first interval as prev
+ 
+//  for(let i = 1; i < intervals.length; i++){
+//      if(intervals[i][0] < prev[1]) {
+//          console.log(intervals[i])
+//          count++ 
+
+//      } else {
+//       prev = intervals[i]
+//      }
+//  }
+//  return count
+
+console.log(findMinArrowShots([[10,16], [2,4], [1,6], [7,12], [13,14]])) //3
+// console.log(burstBalloons([[10,16], [2,4], [1,6], [7,12]])) //2
+// console.log(burstBalloons([[10,16], [2,8], [1,9], [7,12]])) //2
+// console.log(burstBalloons([[10,16]])) //1
+// console.log(burstBalloons([[1,2], [3,4]])) // 2
+// console.log(burstBalloons([[1,2], [2,4]])) //1
+// console.log(burstBalloons([[-1,2], [3,4]])) //2
 // console.log(burstBalloons([[2,8], [1,6], [7,12], [3, 5]])) 
