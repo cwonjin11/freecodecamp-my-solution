27272a9d2c7b5e167303dd7715682bd2cc8f48d6
Wonjin Cho <cwonjin1@gmail.com>
2022-09-08 13:43:34 -0400

    practice solved

---
+++ self_practice.js/sumUnique.js
@@ -0,0 +1,21 @@
+
+
+const sumOfUniq = (arr) => {
+
+   let hash = {}
+   for(const num of arr){
+    !hash[num] ? hash[num] = 1 : hash[num]++
+   }
+
+   let result = 0
+   for(let key in hash){
+    if(hash[key] == 1){
+        result += +key
+    }
+   }
+   return result
+}
+
+console.log(sumOfUniq([1,2,2,3]))
+console.log(sumOfUniq([1,2,2,2,3,4]))
+console.log(sumOfUniq([1,2,2,2,3,3,4]))
